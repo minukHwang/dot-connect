acc630db6143c25eaf4f0a7a3312f46bbf95bafc
Minuk Hwang <damin2003@yonsei.ac.kr>
2023-03-04 09:27:59 +0900

    second commit

--- src/pages/ReportMain.js
+++ src/pages/ReportMain.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./ReportMain.scss";
 
 const ReportMain = () => {
-  return <div>ReportMain</div>;
+  return (
+    <div>
+      <div className="plus-btn">
+        <div className="icon-holder">
+          <span class="material-symbols-outlined">add</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReportMain;
